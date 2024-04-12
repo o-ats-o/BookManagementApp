013e12138a0b88dc4dca394b2d434e6b5da2f20b
ats <atsushi.k214@gmail.com>
2024-04-12 23:20:28 +0900

    SDKを34にバージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,25 +47,24 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-android:1.6.4")
+    implementation("androidx.compose.ui:ui-android:1.6.5")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     implementation("com.google.ar.sceneform:sceneform-base:1.17.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -85,6 +84,9 @@ dependencies {
     implementation("com.squareup.moshi:moshi:1.15.1")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     implementation("com.squareup.moshi:moshi-adapters:1.15.1")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
--- app/src/main/java/com/example/bookmanagementapp/ui/theme/screens/BookInfoScreen.kt
+++ app/src/main/java/com/example/bookmanagementapp/ui/theme/screens/BookInfoScreen.kt
@@ -1,11 +1,22 @@
 package com.example.bookmanagementapp.ui.theme.screens
 
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 
 @Composable
 fun BookInfoScreen(
@@ -15,21 +26,41 @@ fun BookInfoScreen(
     val bookInfoState = viewMode.bookInfoState.collectAsState()
 
     LaunchedEffect(isbn) {
-        Log.e("BookInfoScreen", "Received ISBN: $isbn") // 追加
         viewMode.getBookInfo(isbn)
     }
 
     val bookInfo = bookInfoState.value
     if (bookInfo != null) {
+        val title = remember { mutableStateOf(bookInfo.title) }
+        val authors = remember { mutableStateOf(bookInfo.authors.joinToString(", ")) }
+        val description = remember { mutableStateOf(bookInfo.description) }
+
         Column {
-            Text(
-                text = "Title: ${bookInfo.title}"
+            Image(
+                painter = rememberAsyncImagePainter(
+                    ImageRequest.Builder(LocalContext.current)
+                        .data(data = bookInfo.imageLinks.thumbnail).apply(block = fun ImageRequest.Builder.() {
+                            transformations(RoundedCornersTransformation(10f))
+                        }).build()
+                ),
+                contentDescription = "Book cover image",
+                modifier = Modifier.height(200.dp),
+                contentScale = ContentScale.Crop
             )
-            Text(
-                text = "Authors: ${bookInfo.authors.joinToString(", ")}"
+            TextField(
+                value = title.value,
+                onValueChange = { newValue -> title.value = newValue },
+                label = { Text("Title") }
             )
-            Text(
-                text = "Description: ${bookInfo.description}"
+            TextField(
+                value = authors.value,
+                onValueChange = { newValue -> authors.value = newValue },
+                label = { Text("Authors") }
+            )
+            TextField(
+                value = description.value,
+                onValueChange = { newValue -> description.value = newValue },
+                label = { Text("Description") }
             )
         }
     }
