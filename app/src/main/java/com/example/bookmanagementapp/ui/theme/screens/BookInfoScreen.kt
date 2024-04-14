f868ffd3842e77929ea189b59139033d4fd066f6
ats <atsushi.k214@gmail.com>
2024-04-15 00:35:42 +0900

    コードの構造の修正

--- app/src/main/java/com/example/bookmanagementapp/ui/theme/screens/BookInfoScreen.kt
+++ app/src/main/java/com/example/bookmanagementapp/ui/theme/screens/BookInfoScreen.kt
@@ -1,20 +1,23 @@
 package com.example.bookmanagementapp.ui.theme.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -22,6 +25,8 @@ import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.RoundedCornersTransformation
+import com.example.bookmanagementapp.model.BookInfo
+import com.example.bookmanagementapp.model.ImageLinks
 
 @Composable
 fun BookInfoScreen(
@@ -37,60 +42,72 @@ fun BookInfoScreen(
 
     val bookInfo = bookInfoState.value
     if (bookInfo != null) {
-        val title = remember { mutableStateOf(bookInfo.title ?: "") }
-        val authors = remember { mutableStateOf(bookInfo.authors?.joinToString(", ") ?: "") }
-        val description = remember { mutableStateOf(bookInfo.description ?: "") }
-        val pageCount = remember { mutableStateOf(bookInfo.pageCount?.toString() ?: "") }
-
-        Column (
-            modifier = modifier
-                .padding(18.dp)
-                .padding(top = 24.dp)
-        ) {
-            bookInfo.imageLinks?.let { imageLinks ->
-                Image(
-                    painter = rememberAsyncImagePainter(
-                        ImageRequest.Builder(LocalContext.current)
-                            .data(data = imageLinks.thumbnail).apply(block = fun ImageRequest.Builder.() {
-                                transformations(RoundedCornersTransformation(10f))
-                            }).build(),
-                    ),
-                    contentDescription = "Book cover image",
-                    modifier = Modifier
-                        .height(240.dp)
-                        .width(160.dp)
-                        .align(CenterHorizontally),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = title.value,
-                onValueChange = { newValue -> title.value = newValue },
-                label = { Text("タイトル") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = authors.value,
-                onValueChange = { newValue -> authors.value = newValue },
-                label = { Text("著者") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = description.value,
-                onValueChange = { newValue -> description.value = newValue },
-                label = { Text("書籍概要") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = pageCount.value,
-                onValueChange = { newValue -> description.value = newValue },
-                label = { Text("総ページ数") },
-                modifier = Modifier.fillMaxWidth()
-            )
+        Column(modifier = modifier) {
+            BookInfoLayout(bookInfo)
         }
+    }
+}
+
+@Composable
+fun BookCover(
+    imageLinks: ImageLinks?,
+    modifier: Modifier = Modifier
+) {
+    imageLinks?.let { links ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = rememberAsyncImagePainter(
+                    ImageRequest.Builder(LocalContext.current)
+                        .data(data = links.thumbnail).apply(block = fun ImageRequest.Builder.() {
+                            transformations(RoundedCornersTransformation(10f))
+                        }).build(),
+                ),
+                contentDescription = "Book cover image",
+                modifier
+                    .height(240.dp)
+                    .width(160.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+
+        }
+    }
+}
+
+@Composable
+fun BookField(value: MutableState<String>, label: String, onValueChange: (String) -> Unit) {
+    TextField(
+        value = value.value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun BookInfoLayout(bookInfo: BookInfo?, modifier: Modifier = Modifier) {
+    val title = remember { mutableStateOf(bookInfo?.title?: "") }
+    val authors = remember { mutableStateOf(bookInfo?.authors?.joinToString(", ") ?: "") }
+    val description = remember { mutableStateOf(bookInfo?.description?: "") }
+    val pageCount = remember { mutableStateOf(bookInfo?.pageCount?.toString()?: "") }
+
+    LazyColumn (
+        modifier = modifier
+            .padding(18.dp)
+            .padding(top = 24.dp)
+    ) {
+        item { BookCover(bookInfo?.imageLinks) }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item { BookField(title, "タイトル") { newValue -> title.value = newValue } }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item { BookField(authors, "著者") { newValue -> authors.value = newValue } }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item { BookField(description, "書籍概要") { newValue -> description.value = newValue } }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item { BookField(pageCount, "総ページ数") { newValue -> pageCount.value = newValue } }
     }
 }
