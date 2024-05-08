643f007c0ac0fb12902c19f7f4c644a3480a8197
ats <atsushi.k214@gmail.com>
2024-05-08 22:44:35 +0900

    style: 書籍タイトルと著者の表示を1行に制限かつ長い場合は末に[...]を表示.

--- app/src/main/java/com/example/bookmanagementapp/view/screen/BookListScreen.kt
+++ app/src/main/java/com/example/bookmanagementapp/view/screen/BookListScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +68,7 @@ fun BookItemCard(
                 painter = rememberAsyncImagePainter(model = book.thumbnail),
                 contentDescription = "Book thumbnail",
                 modifier = Modifier
-                    .height(100.dp)
+                    .height(120.dp)
                     .width(80.dp)
                     .padding(start = 10.dp, end = 5.dp, top = 5.dp, bottom = 5.dp)
             )
@@ -75,19 +76,23 @@ fun BookItemCard(
             Column {
                 Text(
                     text = book.title.toString(),
-                    modifier = Modifier.padding(start = 6.dp, top = 6.dp),
-                    fontSize = 18.sp
+                    modifier = Modifier.padding(start = 6.dp, top = 6.dp, end = 6.dp),
+                    fontSize = 18.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text( // 著者情報を表示するTextコンポーネントを追加
                     text = book.authors.toString(),
-                    modifier = Modifier.padding(start = 6.dp),
-                    fontSize = 14.sp
+                    modifier = Modifier.padding(start = 6.dp, end = 6.dp),
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     text = "${(progress * 100).toInt()}%",
                     modifier = Modifier.padding(start = 6.dp)
                 )
-                // Display a progress bar that represents the reading progress
+                // 進捗状況を示すLinearProgressIndicatorを追加
                 LinearProgressIndicator(
                     progress = { progress },
                     modifier = Modifier.fillMaxWidth(),
@@ -108,7 +113,7 @@ fun PreviewBookItemCard(){
             description = "説明",
             pageCount = 100,
             thumbnail = "https://books.google.com/books/content?id=1l8qAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-            readPageCount = 50
+            readPageCount = 100
         ),
         progress = 0.5f
     )
