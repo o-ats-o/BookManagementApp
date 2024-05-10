958bdd4629f0da9f93dff8f847d19321b3a79a18
ats <atsushi.k214@gmail.com>
2024-05-11 00:48:28 +0900

    fix: GetBookInfoUseCaseの実装を間違っていたのを修正（APIから書籍情報を取得すべきとこをローカルDBから行っていた）

--- app/src/main/java/com/example/bookmanagementapp/usecase/GetBookInfoUsecase.kt
+++ app/src/main/java/com/example/bookmanagementapp/usecase/GetBookInfoUsecase.kt
@@ -14,6 +14,24 @@ class GetBookInfoUseCaseImpl @Inject constructor(
     private val bookRepository: BookRepository
 ) : GetBookInfoUseCase {
     override suspend fun execute(isbn: String): BookInfoEntity? {
-        return bookRepository.getBookInfo(isbn)
+        // Google Books APIから書籍情報を取得
+        val response = bookRepository.getBookInfoFromApi(isbn)
+        if (response.isSuccessful) {
+            // レスポンスからBookInfoEntityを作成して返す
+            val bookResponse = response.body()
+            return bookResponse?.items?.get(0)?.volumeInfo?.let { volumeInfo ->
+                BookInfoEntity(
+                    isbn = isbn,
+                    title = volumeInfo.title,
+                    authors = volumeInfo.authors?.joinToString(", "),
+                    description = volumeInfo.description,
+                    pageCount = volumeInfo.pageCount,
+                    thumbnail = volumeInfo.imageLinks?.thumbnail,
+                    readPageCount = 0
+                )
+            }
+        } else {
+            return null
+        }
     }
 }
