ac662940aa60eb92b898835ad34128436c433346
ats <atsushi.k214@gmail.com>
2024-12-07 14:16:15 +0900

    API叩くときにisbn:[isbn13]ではなくクエリに直接指定するようにした（前まではこれでうまくいっていたが，API側の問題な気がする）

--- app/src/main/java/com/example/bookmanagementapp/usecase/GetBookInfoUsecase.kt
+++ app/src/main/java/com/example/bookmanagementapp/usecase/GetBookInfoUsecase.kt
@@ -17,7 +17,7 @@ class GetBookInfoUseCaseImpl
     ) : GetBookInfoUseCase {
         override suspend fun execute(isbn: String): BookInfoEntity? {
             // Google Books APIから書籍情報を取得
-            val response = bookRepository.getBookInfoFromApi("isbn:$isbn")
+            val response = bookRepository.getBookInfoFromApi(isbn)
             if (response.isSuccessful) {
                 // レスポンスからBookInfoEntityを作成して返す
                 val bookResponse = response.body()
